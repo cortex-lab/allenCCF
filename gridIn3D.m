dffca700e38cf18097c1e28418497506beafc651
nsteinme <nick.steinmetz@gmail.com>
2017-04-13 12:45:34 +0100

    return handles in gridIn3D

--- gridIn3D.m
+++ gridIn3D.m
@@ -1,6 +1,6 @@
 
 
-function gridIn3D(volData, contourHeight, sliceSpacing, origin)
+function contourHands = gridIn3D(volData, contourHeight, sliceSpacing, origin)
 
 hold on;
 
@@ -8,6 +8,7 @@ hold on;
 % xSlices = round(sliceSpacing/2):sliceSpacing:size(volData,1);
 xSlices = origin(1)+(-1000:1000)*sliceSpacing;
 xSlices = xSlices(xSlices>0 & xSlices<=size(volData,1));
+contourHands{1} = {};
 for x = xSlices
     thisSlice = squeeze(volData(x,:,:));
     
@@ -16,10 +17,27 @@ for x = xSlices
     cP = parseContours(c);
     
     % plot the contours    
-    cellfun(@(yz)plot3(x*ones(1,size(yz,2)), yz(1,:), yz(2,:), 'Color', [0 0 0 0.3]), cP);    
+    contourHands{1}{end+1} = cellfun(@(yz)plot3(x*ones(1,size(yz,2)), yz(1,:), yz(2,:), 'Color', [0 0 0 0.3]), cP, 'uni', false);    
 end
 
-% repeat for the other two directions
+% contours at one ML location, i.e. sagittal slices
+% xSlices = round(sliceSpacing/2):sliceSpacing:size(volData,3);
+xSlices = origin(3)+(-1000:1000)*sliceSpacing;
+xSlices = xSlices(xSlices>0 & xSlices<=size(volData,3));
+contourHands{2} = {};
+for x = xSlices
+    thisSlice = squeeze(volData(:,:,x));
+    
+    % compute the contours
+    c = contourc(thisSlice, contourHeight*[1 1]);
+    cP = parseContours(c);
+    
+    % plot the contours    
+    contourHands{2}{end+1} = cellfun(@(yz)plot3(yz(2,:), x*ones(1,size(yz,2)), yz(1,:),  'Color', [0 0 0 0.3]), cP, 'uni', false);    
+end
+
+% Have elected to leave out the third possible axis - looks worse including
+% this
 % for x = round(sliceSpacing/2):sliceSpacing:size(volData,2)
 %     thisSlice = squeeze(volData(:,x,:));
 %     
@@ -30,21 +48,6 @@ end
 %     % plot the contours    
 %     cellfun(@(yz)plot3(yz(2,:), yz(1,:), x*ones(1,size(yz,2)),  'Color', [0 0 0 0.3]), cP);    
 % end
-
-% contours at one ML location, i.e. sagittal slices
-% xSlices = round(sliceSpacing/2):sliceSpacing:size(volData,3);
-xSlices = origin(3)+(-1000:1000)*sliceSpacing;
-xSlices = xSlices(xSlices>0 & xSlices<=size(volData,3));
-for x = xSlices
-    thisSlice = squeeze(volData(:,:,x));
-    
-    % compute the contours
-    c = contourc(thisSlice, contourHeight*[1 1]);
-    cP = parseContours(c);
-    
-    % plot the contours    
-    cellfun(@(yz)plot3(yz(2,:), x*ones(1,size(yz,2)), yz(1,:),  'Color', [0 0 0 0.3]), cP);    
-end
 
 xlabel('x'); ylabel('y'); zlabel('z');
 
