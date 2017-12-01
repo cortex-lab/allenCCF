49146f9d32178a88f6a0fdb4c62af9cf337cd17b
nsteinme <nick.steinmetz@gmail.com>
2017-12-01 19:24:47 +0000

    updated colormap

--- allen_ccf_colormap.m
+++ allen_ccf_colormap.m
@@ -1,5 +1,10 @@
-function cmap = allen_ccf_colormap()
+function cmap = allen_ccf_colormap(varargin)
 % colormap for allen annotations "by index". The mat file with the colormap
 % must be int he same directory as this function. 
 p = mfilename('fullpath');
-load(fullfile(fileparts(p), 'allen_ccf_colormap.mat'));
+
+if nargin>0 && strcmp(varargin{1}, '2017')
+    load(fullfile(fileparts(p), 'allen_ccf_colormap_2017.mat'));
+else    
+    load(fullfile(fileparts(p), 'allen_ccf_colormap.mat'));
+end
