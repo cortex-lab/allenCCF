94e4af4a64ecbc7c2849f2fd60bf0688049a99b5
Nick Steinmetz <nick.steinmetz@gmail.com>
2018-07-25 13:54:37 +0100

    slice with outlines

---
+++ Browsing Functions/sliceOutlineWithRegion.m
@@ -0,0 +1,38 @@
+
+
+function im = sliceOutlineWithRegion(avSlice, regInd, regColor, ax)
+
+if nargin<2
+    regInd = [];
+    regColor = [];
+end
+if nargin<4
+    ax = [];
+end
+
+diffilt = -1/8*ones(3,3);
+diffilt(2,2) = 1;
+thisd = conv2(avSlice, diffilt, 'same');
+
+thisbw = abs(sign(thisd));
+se = strel('square', 2);
+im = imerode(thisbw, se); % im is size of avSlice, 0 for bckg and 1 for borders
+im(end,:) = 0; im(:,end) = 0; % these were ones 
+
+if ~isempty(regInd) % specified a region to highlight
+    highlightPts = avSlice==regInd & im==0;
+    im = uint8(~im*255);
+    im1 = im; im1(highlightPts)=uint8(regColor(1)*255);
+    im2 = im; im2(highlightPts)=uint8(regColor(2)*255);
+    im3 = im; im3(highlightPts)=uint8(regColor(3)*255);
+    im = cat(3, im1, im2, im3);    
+    
+else
+    im = repmat(uint8(~im*255), [1,1,3]);
+end
+
+if ~isempty(ax)
+    image(ax, im);
+    axis image
+    axis off
+end
---
+++ Browsing Functions/sliceOutlineWithRegionVec.m
@@ -0,0 +1,73 @@
+
+
+function [coords, coordsReg] = sliceOutlineWithRegionVec(avSlice, regInd, regColor, ax)
+
+if nargin<2
+    regInd = [];
+    regColor = [];
+end
+if nargin<4
+    ax = [];
+end
+
+if ~isempty(regInd) % specified a region to highlight
+    % do this first so it's in the background of the outlines
+    c = contourc(double(avSlice==regInd), [0.5 0.5]);
+    coordsReg = makeSmoothCoords(c);    
+    
+    if ~isempty(ax)
+        for cidx = 1:numel(coordsReg)
+            fill(ax, coordsReg(cidx).x,coordsReg(cidx).y, regColor); hold on;                   
+        end
+    end
+else
+    coordsReg = [];
+end
+
+
+im = sliceOutlineWithRegion(avSlice);
+
+c = contourc(double(im(:,:,1)), [128 128]);
+
+% now want to smooth the contours and drop the really small ones. 
+coords = makeSmoothCoords(c);
+
+if ~isempty(ax)
+    for cidx = 1:numel(coords)
+        plot(ax, coords(cidx).x,coords(cidx).y, 'k','LineWidth', 2.0); hold on;    
+        
+        % using a fill looks nicer but doesn't save to pdf very well
+        %fill(x,y, cmap(uAnn(u),:), 'FaceAlpha', 0.2, 'EdgeAlpha', 0);
+    end
+    axis(ax,'image');
+    axis(ax,'off');
+    set(ax, 'YDir', 'reverse'); 
+end
+
+
+
+return;
+
+function coords = makeSmoothCoords(c)
+
+coords = [];
+ii = 1;coordsInd = 1; 
+buff = 10;
+while ii<size(c,2)
+    n = c(2,ii);
+    if n>100
+        x = c(1,ii+1:ii+n);
+        y = c(2,ii+1:ii+n);
+        x = [x(end-buff:end) x x(1:buff)]; % buffer makes ends meet
+        y = [y(end-buff:end) y y(1:buff)];
+        x = smooth(x,25,'loess');
+        y = smooth(y,25,'loess');
+        x = [x(buff+1:end-buff-1);x(buff+1)];
+        y = [y(buff+1:end-buff-1);y(buff+1)];
+        coords(coordsInd).x = x;
+        coords(coordsInd).y = y;
+        coordsInd = coordsInd+1;                                        
+        
+    end
+    ii = ii+n+1;
+end
