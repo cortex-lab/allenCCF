4fa077d91051c716a76986c9deedb19b43ec15e6
Nick Steinmetz <nick.steinmetz@gmail.com>
2019-12-18 18:45:09 -0800

    update addCtxOutlines

--- Browsing Functions/addAllenCtxOutlines.m
+++ Browsing Functions/addAllenCtxOutlines.m
@@ -1,29 +1,31 @@
 
 
-function addAllenCtxOutlines(bregma, lambda, lineColor)
+function addAllenCtxOutlines(bregma, lambda, lineColor, pixSize)
 % to get bregma/lambda, do:
 % >> figure; imagesc(myImg); axis image; 
 % then use the cursor to click on bregma, then swap the coords (if cursor
 % says X=xx and Y=yy, then bregma is [yy, xx]).
 % or leave blank to get prompted with ginput. 
 % lambda just needs to be on the midline posterior to bregma.
-
 if isempty(bregma)
     fprintf(1, 'click bregma\n');
     [x,y] = ginput(1);
-    bregma = [y,x];
+    bregma = [y,x]
     fprintf(1, 'click lambda\n');
     [x,y] = ginput(1);
-    lambda = [y,x];
+    lambda = [y,x]
 end
 
 apDir = lambda-bregma; apDir = apDir./norm(apDir);
 
-pixSize = 0.0217; % mm/pix. This is for PCO edge 5.5 with 0.6x mag (as kilotrode)
+if nargin<4
+    pixSize = 0.0217; % mm/pix. This is for PCO edge 5.5 with 0.6x mag (as kilotrode)
+end
 
 ccfbregma = allenCCFbregma()/100/pixSize;
 
-load('J:\allen\ctxOutlines.mat');
+
+load(fullfile(fileparts(mfilename('fullpath')), 'ctxOutlines.mat'));
 
 hold on; 
 for q = 1:numel(coords) % coords is from ctxOutlines.mat
